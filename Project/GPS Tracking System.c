0f1ee2a377f2746ebc9ebbcd801440bf80fea64e
yoyosef74 <84831859+yoyosef74@users.noreply.github.com>
2021-06-04 11:17:37 +0200

    Initialization of Ports F , A & B

--- Project/GPS Tracking System.c
+++ Project/GPS Tracking System.c
@@ -1,8 +1,55 @@
 #include "tm4c123gh6pm.h"
 
+void portF_init()
+{
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	while((SYSCTL_PRGPIO_R & 0x20) == 0);
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;
+	GPIO_PORTF_CR_R = 0x02;
+	GPIO_PORTF_AFSEL_R = 0;
+	GPIO_PORTF_AMSEL_R = 0;
+	GPIO_PORTF_PCTL_R = 0;
+	GPIO_PORTF_DIR_R = 0x02;
+	GPIO_PORTF_DEN_R = 0x02;
+	GPIO_PORTF_PUR_R = 0;
+	
+	
+}
+
+void portA_init()
+{
+	SYSCTL_RCGCGPIO_R |= 0x01;
+	while((SYSCTL_PRGPIO_R & 0x01) == 0);
+	GPIO_PORTA_CR_R = 0x70;
+	GPIO_PORTA_AFSEL_R = 0;
+	GPIO_PORTA_AMSEL_R = 0;
+	GPIO_PORTA_PCTL_R = 0;
+	GPIO_PORTA_DIR_R = 0x7F;
+	GPIO_PORTA_DEN_R = 0x7F;
+	GPIO_PORTA_PUR_R = 0;
+	
+	
+}
+
+void portB_init()
+{
+	SYSCTL_RCGCGPIO_R |= 0x02;
+	while((SYSCTL_PRGPIO_R & 0x02) == 0);
+	GPIO_PORTB_CR_R = 0x7F;
+	GPIO_PORTB_AFSEL_R = 0;
+	GPIO_PORTB_AMSEL_R = 0;
+	GPIO_PORTB_PCTL_R = 0;
+	GPIO_PORTB_DIR_R = 0x7F;
+	GPIO_PORTB_DEN_R = 0x7F;
+	GPIO_PORTB_PUR_R = 0;
+	
+	
+}
+
 int main()
 {
-	initPortA();
-	initPortB();
+	portF_init();
+	portA_init();
+	portB_init();
 
 }
