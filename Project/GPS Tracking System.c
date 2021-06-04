a78242d5d711cf5f625790ec48afada941022869
ahmedsnosy1902171 <19021712@eng.asu.edu.eg>
2021-06-04 16:42:58 +0200

    Digits Intialization

--- Project/GPS Tracking System.c
+++ Project/GPS Tracking System.c
@@ -1,5 +1,17 @@
 #include "tm4c123gh6pm.h"
 
+# define zero 0x3F
+# define one 0x06
+# define two 0x5B
+# define three 0x4F
+# define four 0x66
+# define five 0x6D
+# define six 0x7D
+# define seven 0x07
+# define eight 0x7F
+# define nine 0x6F
+
+
 void portF_init()
 {
 	SYSCTL_RCGCGPIO_R |= 0x20;
@@ -44,6 +56,41 @@ void portB_init()
 	GPIO_PORTB_PUR_R = 0;
 	
 	
+}
+void digit(int digit)
+{
+		switch (digit){
+			case 0:
+				GPIO_PORTB_DATA_R = zero;
+			  break;
+			case 1:
+				GPIO_PORTB_DATA_R = one;
+			  break;
+			case 2:
+				GPIO_PORTB_DATA_R = two;
+			  break;
+			case 3:
+				GPIO_PORTB_DATA_R = three;
+			  break;
+			case 4:
+				GPIO_PORTB_DATA_R = four;
+			  break;
+			case 5:
+				GPIO_PORTB_DATA_R = five;
+			  break;
+			case 6:
+				GPIO_PORTB_DATA_R = six;
+			  break;
+			case 7:
+				GPIO_PORTB_DATA_R = seven;
+			  break;
+			case 8:
+				GPIO_PORTB_DATA_R = eight;
+			  break;
+			case 9:
+				GPIO_PORTB_DATA_R = nine;
+			  break;
+		}
 }
 
 int main()
